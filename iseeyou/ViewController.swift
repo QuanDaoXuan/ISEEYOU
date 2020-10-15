ea329fb3fd3823ee9649402cf53bef3e3f4dee93
quandx <quandx@kaopiz.com>
2020-10-15 17:18:12 +0700

    Initial Commit

---
+++ iseeyou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iseeyou
+//
+//  Created by resopt on 7/24/1399 AP.
+//  Copyright © 1399 truc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iseeyouTests/iseeyouTests.swift
@@ -0,0 +1,34 @@
+//
+//  iseeyouTests.swift
+//  iseeyouTests
+//
+//  Created by resopt on 7/24/1399 AP.
+//  Copyright © 1399 truc. All rights reserved.
+//
+
+import XCTest
+@testable import iseeyou
+
+class iseeyouTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
