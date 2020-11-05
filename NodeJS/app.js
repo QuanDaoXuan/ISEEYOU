f0dce6a9535c07468b31a318caeb8ca013d74839
quandx <quandx@kaopiz.com>
2020-11-05 21:57:20 +0700

    edit server

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -40,66 +40,74 @@ app.post("/login", jsonParse, function (req, res) {
   });
   var sqlString = `SELECT * FROM Users WHERE user_name = '${req.body.username}' and password = '${req.body.password}'`;
   console.log(sqlString);
-  connect.connect(function (err) {
-    if (err) throw err;
-    connect.query(sqlString, function (error, rows) {
-      if (rows.length == 1) {
-        res.json(
-          {
-            message: "đăng nhập thành công !!",
-          },
-          200
-        );
-      } else {
-        res.json(
-          {
-            message: "sai thông tin đăng nhập !!",
-          },
-          403
-        );
-      }
-    });
-    connect.end();
+  // connect.connect(function (err) {
+  //   if (err) throw err;
+  //   connect.query(sqlString, function (error, rows) {
+  //     if (rows.length == 1) {
+  //       res.json(
+  //         {
+  //           message: "đăng nhập thành công !!",
+  //         },
+  //         200
+  //       );
+  //     } else {
+  //       res.json(
+  //         {
+  //           message: "sai thông tin đăng nhập !!",
+  //         },
+  //         403
+  //       );
+  //     }
+  //   });
+  //   connect.end();
+  // });
+  connect.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
   });
 });
 
-app.post("/register", jsonParse, function (req, res) {
-  var connect = mysql.createConnection({
-    host: localhost,
-    user: user,
-    password: password,
-    database: database,
-  });
-  var sqlString = `SELECT * FROM Users WHERE user_name = '${req.body.username}'`;
-  console.log(sqlString);
-  connect.connect(function (err) {
-    if (err) throw err;
-    connect.query(sqlString, function (error, rows) {
-      if (rows.length === 0) {
-        var sqlInsertString = `INSERT INTO Users (id, user_name,password) VALUES (1,'${req.body.username}', '${req.body.username}')`;
-        console.log(sqlInsertString);
-        connect.query(sqlInsertString, function (error, rows) {
-          console.log(error);
-          res.json(
-            {
-              message: "Đăng ký thành công!!",
-            },
-            200
-          );
-          connect.end();
-        });
-      } else {
-        res.json(
-          {
-            message: "Đăng ký thất bại !!",
-          },
-          403
-        );
-        connect.end();
-      }
-    });
-  });
-});
+// app.post("/register", jsonParse, function (req, res) {
+//   var connect = mysql.createConnection({
+//     host: localhost,
+//     user: user,
+//     password: password,
+//     database: database,
+//   });
+//   var sqlString = `SELECT * FROM Users WHERE user_name = '${req.body.username}'`;
+//   console.log(sqlString);
+//   connect.connect(function (err) {
+//     if (err) throw err;
+//     connect.query(sqlString, function (error, rows) {
+//       if (rows.length === 0) {
+//         var sqlInsertString = `INSERT INTO Users (id, user_name,password) VALUES (1,'${req.body.username}', '${req.body.username}')`;
+//         console.log(sqlInsertString);
+//         connect.query(sqlInsertString, function (error, rows) {
+//           console.log(error);
+//           res.json(
+//             {
+//               message: "Đăng ký thành công!!",
+//             },
+//             200
+//           );
+//           connect.end();
+//         });
+//       } else {
+//         res.json(
+//           {
+//             message: "Đăng ký thất bại !!",
+//           },
+//           403
+//         );
+//         connect.end();
+//       }
+//     });
+//   });
+// });
 
 app.listen(port, function () {
   console.log("server is listening on port ", port);
